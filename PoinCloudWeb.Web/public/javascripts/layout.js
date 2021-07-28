9de1629e138c89507ac9390860faa1957cb38e3c
Tim Wundenberg <timwundenberg@outlook.de>
2021-07-28 18:01:21 +0200

    remove express

--- PoinCloudWeb.Web/public/javascripts/layout.js
+++
@@ -1,9 +0,0 @@
-function onClickMain() {
-    window.location.href = "/";
-}
-function onClickScanner() {
-    window.location.href = "/scanner";
-}
-function onClickMap() {
-    window.location.href = "/map";
-}
