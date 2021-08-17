d03acddb677c18b0bf5ad14a271c4fd6bad958e0
Tim Wundenberg <timwundenberg@outlook.de>
2021-08-17 21:48:19 +0200

    add rotation and transformation of PointClouds in map-view

--- PointCloudWeb.Web/src/components/ScanItem.vue
+++ PointCloudWeb.Web/src/components/ScanItem.vue
@@ -1,21 +1,32 @@
 <template>
   <div>
-    <font-awesome-icon class="icon" :icon="iconName" @click="onClickVisible" />
+    <font-awesome-icon class="icon" :icon="iconName" @click="onClickVisible"/>
     <p class="caption" @click="onClickEdit()">{{ item.name }}</p>
     <div id="settings" ref="settings" class="collapsed">
       <div id="settings-container" ref="settings-container">
         <div>
           <input
-            ref="focusElement"
-            type="text"
-            v-model="editPcName"
-            @keyup.enter="onEnter()"
+              ref="focusElement"
+              type="text"
+              v-model="editPcName"
+              @keyup.enter="onEnter()"
           />
-
           <button class="button-delete" @click="onClickDelete()">
             <font-awesome-icon class="icon" icon="trash"></font-awesome-icon>
             <span class="button-text">Delete</span>
           </button>
+
+          <p class="data-caption">Rotation</p>
+
+          <input class="input-num" type="number" v-model="editRotationX" @keyup.enter="onEnter()"/>
+          <input class="input-num" type="number" v-model="editRotationY" @keyup.enter="onEnter()"/>
+          <input class="input-num" type="number" v-model="editRotationZ" @keyup.enter="onEnter()"/>
+
+          <p class="data-caption">Transformation</p>
+          <input class="input-num" type="number" v-model="editTransformationX" @keyup.enter="onEnter()"/>
+          <input class="input-num" type="number" v-model="editTransformationY" @keyup.enter="onEnter()"/>
+          <input class="input-num" type="number" v-model="editTransformationZ" @keyup.enter="onEnter()"/>
+
 
           <button @click="onClickSave()">
             <font-awesome-icon class="icon" icon="edit"></font-awesome-icon>
@@ -41,6 +52,12 @@ export default {
       isVisible: true,
       isCollapsed: true,
       editPcName: "",
+      editRotationX: 0,
+      editRotationY: 0,
+      editRotationZ: 0,
+      editTransformationX: 0,
+      editTransformationY: 0,
+      editTransformationZ: 0,
     };
   },
   methods: {
@@ -61,8 +78,14 @@ export default {
         this.$refs.settings.style.height = "0";
       } else {
         this.editPcName = this.item.name;
+        this.editRotationX = this.item.rotation.x;
+        this.editRotationY = this.item.rotation.y;
+        this.editRotationZ = this.item.rotation.z;
+        this.editTransformationX = this.item.transformation.x;
+        this.editTransformationY = this.item.transformation.y;
+        this.editTransformationZ = this.item.transformation.z;
         this.$refs.settings.style.height =
-          this.outerHeight(this.$refs["settings-container"]) + "px";
+            this.outerHeight(this.$refs["settings-container"]) + "px";
         setTimeout(() => this.$refs.focusElement.focus(), 100);
       }
     },
@@ -70,6 +93,16 @@ export default {
       this.$store.dispatch("pci/updatePointCloud", {
         id: this.item.id,
         name: this.editPcName,
+        rotation: {
+          x: parseFloat(this.editRotationX),
+          y: parseFloat(this.editRotationY),
+          z: parseFloat(this.editRotationZ)
+        },
+        transformation: {
+          x: parseFloat(this.editTransformationX),
+          y: parseFloat(this.editTransformationY),
+          z: parseFloat(this.editTransformationZ)
+        }
       });
       this.onClickEdit();
     },
@@ -112,7 +145,12 @@ export default {
 
 .button-delete {
   display: inline-block;
-  margin-left: 10px;
+  margin-left: 5px;
+}
+
+.input-num {
+  display: inline-block;
+  width: 60px;
 }
 
 .button-text {
@@ -135,5 +173,11 @@ export default {
   border-style: solid;
   border-width: 1px;
   border-color: grey;
+}
+
+.data-caption {
+  font-size: 0.5em;
+  margin-bottom: 0px;
+
 }
 </style>
