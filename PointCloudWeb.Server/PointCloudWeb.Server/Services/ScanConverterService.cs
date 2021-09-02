816bd512877f8c3f30c079444a7062645de90df4
Tim Wundenberg <timwundenberg@outlook.de>
2021-09-02 15:55:53 +0200

    fix ScanConverterService

--- PointCloudWeb.Server/PointCloudWeb.Server/Services/ScanConverterService.cs
+++ PointCloudWeb.Server/PointCloudWeb.Server/Services/ScanConverterService.cs
@@ -7,13 +7,13 @@ namespace PointCloudWeb.Server.Services
     {
         public static Point Transform(ScanDataPoint scan)
         {
-            var alpha = scan.RAX * Math.PI / 180;
+            var alpha = (90 - scan.RAX) * Math.PI / 180;
             var beta = scan.RAY * Math.PI / 180;
 
             var p = new Point(
-                y: (int)(scan.DistanceMM * Math.Sin(alpha)),
+                z: (int)(scan.DistanceMM * Math.Sin(alpha)),
                 x: (int)(scan.DistanceMM * Math.Cos(beta) * Math.Cos(alpha)),
-                z: (int)(scan.DistanceMM * Math.Sin(beta) * Math.Cos(alpha))
+                y: (int)(scan.DistanceMM * Math.Sin(beta) * Math.Cos(alpha))
             );
             return p;
         }
